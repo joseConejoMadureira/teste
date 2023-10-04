a52cc306f6076e94148c6edfdbc9e9eda3dbad8f
joseConejoMadureira <jose.games15@outlook.com>
2023-10-04 15:06:55 -0300

    fundamentos react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import FirstComponent from "./components/FirstComponent";
 import TemplateExpressions from "./components/TemplateExpressions";
 import MyComponent from "./components/MyComponent";
 import Events from "./components/Events";
+import Challenge from "./components/Challenge";
+
 
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <TemplateExpressions />
       <MyComponent />
       <Events />
+      <Challenge/>
     </div>
   );
 }
---
+++ src/components/Challenge.js
@@ -0,0 +1,15 @@
+
+
+const Challenge = () => {
+    const a = 10
+    const b = 15
+    return (
+        <div>
+            <p>A: {a}</p>
+            <p>B: {b}</p>
+            <button onClick={() => console.log(a + b)}>Somar!</button>
+        </div>
+    )
+}
+
+export default Challenge
